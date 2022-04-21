f43f6a2e0463155967a40cfa5a264677c6ebc348
oliabka <oliabka@gmail.com>
2022-04-21 23:21:36 +0300

    local address, specification and database connected

---
+++ app/Models/Item.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Domain\Items\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class Item
+ * @package app\Domain\Items\Models
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $description
+ * @property integer $amount
+ */
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $table = 'items';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'amount',
+    ];
+}
