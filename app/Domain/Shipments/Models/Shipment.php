e6555f084c374f8fae206192f0ca4c20b4e7e780
oliabka <oliabka@gmail.com>
2022-04-28 23:08:11 +0300

    adding second table

---
+++ app/Domain/Shipments/Models/Shipment.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\Shipments\Models;
+
+class Shipment
+{
+
+}
--- database/migrations/2022_04_28_184647_shipments_table.php
+++ database/migrations/2022_04_28_184647_shipments_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('items_test', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items_test');
+        //
     }
 };
